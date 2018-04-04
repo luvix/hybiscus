e7ca0004a7ea7a61191afb6e47dab40351421f28
Leesuk "Theodore" Kim <leesuk.kim425@gmail.com>
2018-04-04 22:55:01 +0900

    build original shell script version

--- bootstrap#bash.sh
+++ bootstrap#bash.sh
@@ -1,8 +1,6 @@
-
-## "Leesuk \"Theodore\" Kim, Researcher in SungKyunKwan Univ.<leesuk.kim.skku@gmail.com>"
-
-## Install AFNI and dependency packages
-### Install prerequisite packages
+#! /bin/sh
+## Install FSI, AFNI and dependency packages
+### setup environment for installation
 apt-get update; apt-get -y upgrade
 apt-get install -y software-properties-common
 add-apt-repository universe
@@ -14,48 +12,10 @@ apt-get update
 apt-get install -y gnome-terminal nautilus      \
                         gnome-icon-theme-symbolic
 
+### new user: hybiscus
+useradd -m hybiscus
+
 ### Make “tcsh” default shell (optional/recommended)
-chsh -s /usr/bin/tcsh
+su hybiscus -c "chsh -s /usr/bin/tcsh"
 
-### Install AFNI binaries
-cd
-curl -O https://afni.nimh.nih.gov/pub/dist/bin/linux_ubuntu_16_64/@update.afni.binaries
-tcsh @update.afni.binaries -package linux_ubuntu_16_64  -do_extras
-
-### Install R
-export R_LIBS=$HOME/R
-echo 'export R_LIBS=$HOME/R' >> ~/.bashrc
-echo $R_LIBS
-tcsh -c "setenv R_LIBS $HOME/R"
-echo 'setenv R_LIBS $HOME/R' >> /etc/csh.cshrc  
-mkdir $HOME/R
-curl -O https://afni.nimh.nih.gov/pub/dist/src/scripts_src/@add_rcran_ubuntu.tcsh
-tcsh -c "tcsh @add_rcran_ubuntu.tcsh && rPkgsInstall -pkgs ALL"
-
-# tcsh -c "sudo /root/abin/rPkgsInstall -pkgs ALL"
-
-### Make AFNI/SUMA profiles
-cp $HOME/abin/AFNI.afnirc $HOME/.afnirc 
-tcsh -c "tcsh suma -update_env"
-### Prepare for Bootcamp
-curl -O https://afni.nimh.nih.gov/pub/dist/edu/data/CD.tgz
-tar xvzf CD.tgz && cd CD
-tcsh -c "tcsh s2.cp.files . ~ && cd .."
-
-### Evaluate setup/system (important!)
-tcsh -c "tcsh afni_system_check.py -check_all > ~/out.afni_system_check.txt"
-
-### Niceify terminal (optional, but goood)
-echo 'set filec' >> /etc/csh.cshrc
-echo 'set autolist' >> /etc/csh.cshrc
-echo 'set nobeep'   >> /etc/csh.cshrc
-echo 'set filec' >> ~/.bashrc
-echo 'set autolist' >> ~/.bashrc
-echo 'set nobeep'   >> ~/.bashrc
-
-echo 'alias ls ls --color=auto' >> /etc/csh.cshrc
-echo 'alias ll ls --color -l'   >> /etc/csh.cshrc
-echo 'alias ls="ls --color"'    >> ~/.bashrc
-echo 'alias ll="ls --color -l"' >> ~/.bashrc
-
-tcsh -c "tcsh echo afni -ver"
+sudo tcsh bootstrap#tcsh.sh
